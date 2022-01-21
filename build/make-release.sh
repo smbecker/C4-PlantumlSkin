e7c4218079209b0dfc879f0751b8dca2e56b23e5
Shaun Becker <smbecker@gmail.com>
2022-01-21 16:50:41 -0500

    Fix font color for internal queues

--- build/make-release.sh
+++ build/make-release.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+USER=$(git config --get remote.origin.url | grep -Po "(?<=git@github\.com:)(.*?)(?=/.+.git)")
+BRANCH=$(git branch --show-current)
 function convert_to_importurl {
     input_fname=$1
     output_fname="$(pwd)/output/${1//\.\.\/src\//}"
 
     echo $input_fname
-    cat $input_fname | sed 's/!include \.*/!includeurl https:\/\/raw.githubusercontent.com\/skleanthous\/C4-PlantumlSkin\/master\/build\/output/' > "$output_fname"
+    cat $input_fname | sed "s/!include \.*/!includeurl https:\/\/raw.githubusercontent.com\/$USER\/C4-PlantumlSkin\/$BRANCH\/build\/output/" > "$output_fname"
 }
 
 for file in $(find ../src -type f); do
